a78f28fce9239c59735fe9a139d87527c08f1410
Woodsy03 <119528792+Woodsy03@users.noreply.github.com>
2024-02-10 20:45:14 +0000

    added sql credentials validator

--- SQL-Integration/TableInitialisor.py
+++ SQL-Integration/TableInitialisor.py
@@ -1,42 +1,80 @@
 import os
 import sqlite3
 import csv
-import dotenv
+from dotenv import load_dotenv
+import mysql.connector 
 
 
-# Attempt to import table or create one if it doesn't exist
-#RulesetDatabase = sqlite3.connect("RulesetDatabase.db")
-#Cursor = RulesetDatabase.cursor()
+
+# Defines local directory to allow for relative pathing across any device running the code
+CurrentDirectory = os.path.dirname(__file__)
+RootDirectory = os.path.join(CurrentDirectory, "..")
 
 # load from SQL.env
-load_dotenv()
-StorageType = os.getenv("StorageType", "CSV")
+env_path = os.path.join('SQL-Integration', 'SQL.env')
+load_dotenv(dotenv_path=env_path)
+StorageType = os.getenv("StorageType")
+
+
 
 
 # Attempt to import table or create one if it doesn't exist
-def AccessTable():
+def CreateDatabaseIfNotExists():
     RulesetDatabase = sqlite3.connect("RulesetDatabase.db")
     Cursor = RulesetDatabase.cursor()
     
     # Create table if it doesn't exist
-    cursor.execute(''' 
+    Cursor.execute(''' 
     CREATE TABLE IF NOT EXISTS FirewallRules
     (
-        IP_Adress TEXT PRIMARY KEY,
-        action TEXT NOT NULL,
-        protocol TEXT NOT NULL, 
+        RuleID INT PRIMARY KEY,
+        IP_Address TEXT,
+        Action TEXT NOT NULL,
+        Protocol TEXT NOT NULL, 
         Weighting INTEGER DEFAULT -1,
         CHECK(action IN ('Allow', 'Deny', 'Bypass'))
         CHECK(protocol IN ('TCP', 'UDP', 'ALL'))
-        CHECK (ip_address ~ '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')
     );
     ''')
     
+
+
+## ############################ ## 
+## reading from table data ONLY ## 
+## ############################ ## 
+
+# Loads csv table into primary memory allowing quick read access
+def OpenCSV(CSVFilePath):
+    print("CSV Format File Storage has been selected")
+    with open('FirewallRules.csv', 'r') as CSVFilePath:
+        TableData = csv.reader(CSVFilePath)
+        for record in TableData:
+            print(record)
+         
+
+# Validates if SQL credentials are valid
+def CheckSQLConnection():
+    print("SQL Storage has been selected")
+    try:
+        mydb = mysql.connector.connect(
+            host = os.getenv("SQLHost"),
+            port = os.getenv("SQLPort"),
+            user = os.getenv("SQLUser"),
+            passwd = os.getenv("SQLPassword"),
+            database = os.getenv("FirewallRules")
+        )
+        print("Valid SQL connection credentials provided")
+        CreateDatabaseIfNotExists() # Creates a table for the firewall rules if it doesn't already exist
+   
+    except:
+        print("Valid SQL connection credentials NOT provided")
+    
     
 
 
-
-
+## ####################################################### ##
+## Purely for validation of new entries and existing data  ##
+## ####################################################### ## 
 
 # Check if an IP is a valid ipv4 address
 def IPChecker(IP_Address):
@@ -49,6 +87,14 @@ def IPChecker(IP_Address):
     
 
 
+## ################## ##
+## Global Access Code ##
+## ################## ##
 
+if StorageType == "SQL":
+    CheckSQLConnection()
+elif StorageType == "CSV":
+    CSVFilePath = os.path.join(RootDirectory, "FirewallRules.csv")
+    OpenCSV(CSVFilePath)
 
 
