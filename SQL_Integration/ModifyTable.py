0cb500b5a467d292c7b7fbe7ff6eb9585065b185
Woodsy03 <github@enviromc.com>
2024-05-04 18:38:59 +0100

    Comments

--- SQL_Integration/ModifyTable.py
+++ SQL_Integration/ModifyTable.py
@@ -3,12 +3,13 @@ class ModifyTable:
         self.StorageType = StorageType
         self.cursor = cursor
 
+    # Inserts row into the database, currently natively supports SQL but can relatively easily be expanded to support CSV's, Flatfiles, etc
     class InsertRow:
         def __init__(self, parent):
             self.parent = parent
 
+        # Inserts a row via SQL. Requires a pre-existing cursor object to be connected to the database AND validation externally or from the adjacent "insert" class that the .env file is set to SQL
         def InsertRowSQL(self, ip, allow_deny, protocol, weighting):
-            # Use a parameterized query to safely insert data
             query = """
                 INSERT INTO firewall_rules (IP, AllowDeny, Protocol, Weighting)
                 VALUES (%s, %s, %s, %s)
@@ -21,5 +22,5 @@ class ModifyTable:
             if self.parent.StorageType == "SQL":
                 self.InsertRowSQL(ip, allow_deny, protocol, weighting)
             else:
-                # Add handling for CSV or other storage types if necessary
+                # Placeholder for CSV/other formats. Not able to proceed without code from others.
                 pass
