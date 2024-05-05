ee84a0580086a364bf8c60f70354f4154b6fab05
LMHedges <126117414+LMHedges@users.noreply.github.com>
2024-05-05 04:07:08 +0100

    Update ModifyTable.py
    Not fully implemented but functions are made

--- SQL_Integration/ModifyTable.py
+++ SQL_Integration/ModifyTable.py
@@ -2,7 +2,6 @@ class ModifyTable:
     def __init__(self, StorageType, cursor):
         self.StorageType = StorageType
         self.cursor = cursor
-
     # Inserts row into the database, currently natively supports SQL but can relatively easily be expanded to support CSV's, Flatfiles, etc
     class InsertRow:
         def __init__(self, parent):
@@ -22,6 +21,74 @@ class ModifyTable:
             if self.parent.StorageType == "SQL":
                 self.InsertRowSQL(ip, allow_deny, protocol, weighting)
             else:
-                # Placeholder for CSV/other formats. Not able to proceed without code from others.
+                # Placeholder for CSV/other formats. Not able to proceed without code from others.               
                 pass
-              
+        
+
+        # Inserts a row in FirewallRules.csv
+        def InsertRow_csv(self, IP_Address, Action, Protocol, Weighting):
+            import csv
+            RuleID = 0
+
+            # Gets the last RuleID in FirewallRules
+            with open('FirewallRules.csv', 'r') as file:
+                FirewallRules = csv.reader(file)
+                RuleID = FirewallRules[-5]
+                file.close()
+
+            RuleID+= 1
+            data = (RuleID, IP_Address, Action, Protocol, Weighting)
+
+            with open('FirewallRules.csv', 'w') as file: 
+                FirewallRules = csv.writer(file)
+                FirewallRules.writerow(data)
+                file.close()
+
+
+    # Outputs all the IP addresses in the firewall_rules .csv and .db
+    class OutputIP:
+        def __init__(self, parent):
+            self.parent = parent
+
+        def OutputIP_csv(self):
+            file = open('FirewallRules.csv', 'r')
+            IP_List = []
+            for col in file:
+                IP_List.append(col['IP_Address'])
+            file.close()
+            return IP_List
+        
+        def OutputIP_db(self):
+            query = """
+                SELECT IP_Address FROM firewall_rules
+            """
+            self.parent.cursor.execute(query)
+            IP_List = self.parent.cursor.fetchall()
+            return IP_List
+        
+    # Removes a row based off IP address from the firewall_rules .csv and .db
+    class RemoveRow:
+        def __init__(self, parent):
+            self.parent = parent
+
+        def RemoveRow_csv(self, ip):
+            import csv
+            entries = []
+            with open('FirewallRules.csv', 'r') as file:
+                FirewallRules = csv.reader(file)
+                for row in FirewallRules:
+                    for field in entries:
+                        if field == ip:
+                            entries.remove(row)
+                file.close()
+            with open('FirewallRules.csv', 'w') as file:
+                FirewallRules = csv.writer(file)
+                FirewallRules.writerows(entries)
+
+        def RemoveRow_db(self, ip):
+            query = """
+                DELETE FROM firewall_rules
+                WHERE IP_Address = %s
+            """
+            self.parent.cursor.execute(query, ip)
+            self.parent.cursor.connection.commit()
