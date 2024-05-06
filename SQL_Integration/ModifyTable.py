feb50ba4c672e5a0fec5d907f819290d9afeca73
LMHedges <126117414+LMHedges@users.noreply.github.com>
2024-05-06 15:10:07 +0100

    Update ModifyTable.py
    This should allow for the code to be properly tested

--- SQL_Integration/ModifyTable.py
+++ SQL_Integration/ModifyTable.py
@@ -8,7 +8,7 @@ class ModifyTable:
             self.parent = parent
 
         # Inserts a row via SQL. Requires a pre-existing cursor object to be connected to the database AND validation externally or from the adjacent "insert" class that the .env file is set to SQL
-        def InsertRowSQL(self, ip, allow_deny, protocol, weighting):
+        def InsertRow_db(self, ip, allow_deny, protocol, weighting):
             query = """
                 INSERT INTO firewall_rules (IP, AllowDeny, Protocol, Weighting)
                 VALUES (%s, %s, %s, %s)
@@ -16,14 +16,6 @@ class ModifyTable:
             data = (ip, allow_deny, protocol, weighting)
             self.parent.cursor.execute(query, data)
             self.parent.cursor.connection.commit()
-
-        def insert(self, ip, allow_deny, protocol, weighting):
-            if self.parent.StorageType == "SQL":
-                self.InsertRowSQL(ip, allow_deny, protocol, weighting)
-            else:
-                # Placeholder for CSV/other formats. Not able to proceed without code from others.               
-                pass
-        
 
         # Inserts a row in FirewallRules.csv
         def InsertRow_csv(self, IP_Address, Action, Protocol, Weighting):
@@ -44,20 +36,18 @@ class ModifyTable:
                 FirewallRules.writerow(data)
                 file.close()
 
+        def insert(self, ip, allow_deny, protocol, weighting):
+            if self.parent.StorageType == "SQL":
+                self.InsertRow_db(ip, allow_deny, protocol, weighting)
+            else:
+                self.InsertRow_csv(ip, allow_deny, protocol, weighting)
+
 
     # Outputs all the IP addresses in the firewall_rules .csv and .db
     class OutputIP:
         def __init__(self, parent):
             self.parent = parent
-
-        def OutputIP_csv(self):
-            file = open('FirewallRules.csv', 'r')
-            IP_List = []
-            for col in file:
-                IP_List.append(col['IP_Address'])
-            file.close()
-            return IP_List
-        
+           
         def OutputIP_db(self):
             query = """
                 SELECT IP_Address FROM firewall_rules
@@ -66,10 +56,32 @@ class ModifyTable:
             IP_List = self.parent.cursor.fetchall()
             return IP_List
         
+        def OutputIP_csv(self):
+            file = open('FirewallRules.csv', 'r')
+            IP_List = []
+            for col in file:
+                IP_List.append(col['IP_Address'])
+            file.close()
+            return IP_List
+        
+        def Output(self):
+            if self.parent.StorageType == "SQL":
+                self.OutputIP_db()
+            else:
+                self.OutputIP_csv()
+        
     # Removes a row based off IP address from the firewall_rules .csv and .db
     class RemoveRow:
         def __init__(self, parent):
             self.parent = parent
+
+        def RemoveRow_db(self, ip):
+            query = """
+                DELETE FROM firewall_rules
+                WHERE IP_Address = %s
+            """
+            self.parent.cursor.execute(query, ip)
+            self.parent.cursor.connection.commit()
 
         def RemoveRow_csv(self, ip):
             import csv
@@ -85,10 +97,8 @@ class ModifyTable:
                 FirewallRules = csv.writer(file)
                 FirewallRules.writerows(entries)
 
-        def RemoveRow_db(self, ip):
-            query = """
-                DELETE FROM firewall_rules
-                WHERE IP_Address = %s
-            """
-            self.parent.cursor.execute(query, ip)
-            self.parent.cursor.connection.commit()
+        def Remove(self, ip):
+            if self.parent.StorageType == "SQL":
+                self.RemoveRow_db(ip)
+            else:
+                self.RemoveRow_csv(ip)
